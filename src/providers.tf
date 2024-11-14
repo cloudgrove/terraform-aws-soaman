c4d1f976744473ed23f4fb13bc813956d6a80eec
Nabil Lehlou <nlehlou@gmail.com>
2024-11-14 16:40:04 +0100

    Add the capability to connect to AWS

---
+++ src/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.3.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
--- src/variables.tf
+++ src/variables.tf
@@ -1,9 +1,24 @@
+variable "aws_access_key" {
+  description = "The AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "The target AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "domain_name" {
   description = "The target domain name"
   type        = string
   default     = "cloudgrove.io"
-}
-
-output "domain_name" {
-  value = var.domain_name
 }
