b7cb942eaad145e66ba21c76ca80154f05502e97
Nabil Lehlou <nlehlou@gmail.com>
2025-01-11 13:59:50 +0100

    Rename the Terraform workspace to 'soaman'

--- src/backend.tf
+++ src/backend.tf
@@ -2,12 +2,8 @@ terraform {
   cloud {
     organization = "cloudgrove"
 
-    # Note:
-    # - There are multiple workspaces tagged as "infrastructure-manager", each one manages a different environment.
-    # - The command `terraform init` lets one select the target Terraform workspace locally.
-    # - In the case of CI/CD, the TF_WORKSPACE environment variable is used to specify the target workspace.
     workspaces {
-      tags = ["infrastructure-manager"]
+      tags = ["soaman"]
     }
   }
 }
